e9eb9ec84648d86c95aab4ce373c22d7f4add909
Liz Harris <lizharris90210@gmail.com>
2019-12-23 22:24:35 -0500

    config mongoDB, server,js, api

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const app = express();
+
+connectDB();
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => res.send("API Running"));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
