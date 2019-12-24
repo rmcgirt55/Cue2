0bf63f12d5b378324a604730910ff4799e92201d
Liz Harris <lizharris90210@gmail.com>
2019-12-23 22:44:13 -0500

    routes set, models started

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 // @route GET api/users
 // @desc Test route
 // @access Public
-router.length('/', (req, res) => res.send('User route'));
+router.get('/', (req, res) => res.send('User route'));
+
+module.exports = router;
