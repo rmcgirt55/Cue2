e5952b2ae128d20f905e57affe0cf8e6f0584503
Liz Harris <lizharris90210@gmail.com>
2020-01-11 17:54:21 -0500

    Login, Register, Landing, Navbar initial setup

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Landing = () => {
+  <section class="landing">
+    <div class="dark-overlay">
+      <div class="landing-inner">
+        <h1 class="x-large">socialConnect</h1>
+        <p class="lead">Create a profile/portfolio, share posts and get help</p>
+        <div class="buttons">
+          <a href="register.html" class="btn btn-primary">
+            Sign Up
+          </a>
+          <a href="login.html" class="btn btn-light">
+            Login
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>;
+};
