4c0809b40b53c550dd339f730e655d40a09503d2
jpearce2014 <jlpearce@icloud.com>
2020-01-18 11:26:21 -0500

    Preliminary conversion to SASS

---
+++ client/build/precache-manifest.6e77c2a502a327a5e643c9d9c51bfe87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fed760df30b90839ac02010bbe999a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c17dc52dc8515c87fdd1",
+    "url": "/static/css/main.f63a79b9.chunk.css"
+  },
+  {
+    "revision": "0f76e2bd8cfc76ef1549",
+    "url": "/static/js/2.8f514c70.chunk.js"
+  },
+  {
+    "revision": "5ec987632bc308bcd1222d7283dbdad9",
+    "url": "/static/js/2.8f514c70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c17dc52dc8515c87fdd1",
+    "url": "/static/js/main.8a73dd08.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
