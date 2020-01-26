5f72f6320fdb68d30f7722bc9c906c2bb9d057d2
Hilary Lamb <hilaryklamb@gmail.com>
2020-01-26 13:33:34 -0500

    btn-primary color adjusted

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState, Fragment } from "react";
-import { Link, withRouter, Redirect } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { createProfile, getCurrentProfile } from "../../actions/profile";
+import React, { useEffect, useState, Fragment } from 'react';
+import { Link, withRouter, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
 
 const CreateProfile = ({
   createProfile,
@@ -11,17 +11,17 @@ const CreateProfile = ({
   history
 }) => {
   const [formData, setFormData] = useState({
-    bandName: "",
-    website: "",
-    location: "",
-    status: "",
-    instruments: "",
-    bio: "",
-    twitter: "",
-    facebook: "",
-    linkedin: "",
-    youtube: "",
-    instagram: ""
+    bandName: '',
+    website: '',
+    location: '',
+    status: '',
+    instruments: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: ''
   });
   const [displaySocialInputs, toggleSocialInputs] = useState(false);
   const {
@@ -48,93 +48,93 @@ const CreateProfile = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getCurrentProfile]);
   return loading && profile === null ? (
-    <Redirect to="/dashboard" />
+    <Redirect to='/dashboard' />
   ) : (
     <Fragment>
-      <h1 className="large text-primary">Create Your Profile</h1>
-      <p className="lead">
-        <i className="fas fa-guitar"></i> Let's get some information to make
+      <h1 className='large text-primary'>Create Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-guitar'></i> Let's get some information to make
         your profile rock!
       </p>
       <small>* = required field</small>
-      <form className="form" onSubmit={e => onSubmit(e)}>
-        <div className="form-group">
-          <select name="status" value={status} onChange={e => onChange(e)}>
-            <option value="0">* Select Professional Status</option>
-            <option value="Full-time-musician">
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <select name='status' value={status} onChange={e => onChange(e)}>
+            <option value='0'>* Select Professional Status</option>
+            <option value='Full-time-musician'>
               Full-time musician/industry professional
             </option>
-            <option value="Part-time-musician">
+            <option value='Part-time-musician'>
               Part-time muscian/industry professional
             </option>
-            <option value="Hobby-musician">Hobby musician</option>
-            <option value="Manager">Artist or Band Manager</option>
-            <option value="Backstage">
+            <option value='Hobby-musician'>Hobby musician</option>
+            <option value='Manager'>Artist or Band Manager</option>
+            <option value='Backstage'>
               Backstage (lighting, audio, stage support)
             </option>
-            <option value="Venue">Venue and event professional</option>
-            <option value="Promotion">Promotion/marketing professional</option>
-            <option value="Other">Other</option>
+            <option value='Venue'>Venue and event professional</option>
+            <option value='Promotion'>Promotion/marketing professional</option>
+            <option value='Other'>Other</option>
           </select>
-          <small className="form-text">Background information</small>
+          <small className='form-text'>Background information</small>
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="text"
-            placeholder="Band or Act Name"
-            name="bandName"
+            type='text'
+            placeholder='Band or Act Name'
+            name='bandName'
             value={bandName}
             onChange={e => onChange(e)}
           />
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="text"
-            placeholder="Band or Act Website"
-            name="website"
+            type='text'
+            placeholder='Band or Act Website'
+            name='website'
             value={website}
             onChange={e => onChange(e)}
           />
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="text"
-            placeholder="Location"
-            name="location"
+            type='text'
+            placeholder='Location'
+            name='location'
             value={location}
             onChange={e => onChange(e)}
           />
-          <small className="form-text">
+          <small className='form-text'>
             City & state suggested (eg. Boston, MA)
           </small>
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="text"
-            placeholder="Instruments"
-            name="instruments"
+            type='text'
+            placeholder='Instruments'
+            name='instruments'
             value={instruments}
             onChange={e => onChange(e)}
           />
-          <small className="form-text">
+          <small className='form-text'>
             Please use comma separated values (eg. guitar, vocals, bongos)
           </small>
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <textarea
-            placeholder="A short bio of yourself"
-            name="bio"
+            placeholder='A short bio of yourself'
+            name='bio'
             value={bio}
             onChange={e => onChange(e)}
           />
-          <small className="form-text">Tell us a little about yourself</small>
+          <small className='form-text'>Tell us a little about yourself</small>
         </div>
 
-        <div className="my-2">
+        <div className='my-2'>
           <button
             onClick={() => toggleSocialInputs(!displaySocialInputs)}
-            type="button"
-            className="btn btn-light"
+            type='button'
+            className='btn btn-light'
           >
             Add Social Network Links
           </button>
@@ -142,56 +142,56 @@ const CreateProfile = ({
         </div>
         {displaySocialInputs && (
           <Fragment>
-            <div className="form-group social-input">
-              <i className="fab fa-twitter fa-2x" />
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x' />
               <input
-                type="text"
-                placeholder="Twitter URL"
-                name="twitter"
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
                 value={twitter}
                 onChange={e => onChange(e)}
               />
             </div>
 
-            <div className="form-group social-input">
-              <i className="fab fa-facebook fa-2x" />
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x' />
               <input
-                type="text"
-                placeholder="Facebook URL"
-                name="facebook"
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
                 value={facebook}
                 onChange={e => onChange(e)}
               />
             </div>
 
-            <div className="form-group social-input">
-              <i className="fab fa-youtube fa-2x" />
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x' />
               <input
-                type="text"
-                placeholder="YouTube URL"
-                name="youtube"
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
                 value={youtube}
                 onChange={e => onChange(e)}
               />
             </div>
 
-            <div className="form-group social-input">
-              <i className="fab fa-linkedin fa-2x" />
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x' />
               <input
-                type="text"
-                placeholder="Linkedin URL"
-                name="linkedin"
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
                 value={linkedin}
                 onChange={e => onChange(e)}
               />
             </div>
 
-            <div className="form-group social-input">
-              <i className="fab fa-instagram fa-2x" />
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x' />
               <input
-                type="text"
-                placeholder="Instagram URL"
-                name="instagram"
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
                 value={instagram}
                 onChange={e => onChange(e)}
               />
@@ -199,8 +199,8 @@ const CreateProfile = ({
           </Fragment>
         )}
 
-        <input type="submit" className="btn btn-primary my-1" />
-        <Link className="btn btn-light my-1" to="/dashboard">
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link className='btn btn-light my-1' to='/dashboard'>
           Go Back
         </Link>
       </form>
